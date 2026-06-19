28f93f9226635fb0d8ce5dfb3ab41e2492f10301
Mai-Elhajeen <maielhajeen2001>
2026-06-19 23:41:20 +0300

    preparing homepage and add a dark mode after then prepare Navbar and Sidebar as a component

--- app/layout.tsx
+++ app/layout.tsx
@@ -25,9 +25,11 @@ export default function RootLayout({
   return (
     <html
       lang="en"
-      className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
+      className={`${geistSans.variable} ${geistMono.variable} h-full antialiased dark`}
     >
-      <body className="min-h-full flex flex-col">{children}</body>
+      <body className={`bg-dark-2 ${geistSans.variable} ${geistMono.variable}`}>
+        {children}
+      </body>
     </html>
   );
 }
