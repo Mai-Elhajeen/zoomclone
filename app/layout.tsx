f745fe707298061c75feea2bf6a98548b3e61e2a
Mai-Elhajeen <maielhajeen2001>
2026-06-21 16:02:07 +0300

    replace a font-family

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,14 +1,8 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+import { Inter } from "next/font/google";
 import "./globals.css";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
+const inter = Inter({
   subsets: ["latin"],
 });
 
@@ -28,9 +22,9 @@ export default function RootLayout({
   return (
     <html
       lang="en"
-      className={`${geistSans.variable} ${geistMono.variable} h-full antialiased dark`}
+      className={`h-full antialiased dark`}
     >
-      <body className={`bg-dark-2 ${geistSans.variable} ${geistMono.variable}`}>
+      <body className={`bg-dark-2 ${inter.className} h-full font-sans text-white`}>
         {children}
       </body>
     </html>
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -61,7 +61,7 @@ const config: Config = {
         sm: "calc(var(--radius) - 4px)",
       },
       fontFamily: {
-        sans: ["Inter", ...defaultTheme.fontFamily.sans],
+        sans: ['var(--font-inter)'],
       },
       keyframes: {
         "accordion-down": {
