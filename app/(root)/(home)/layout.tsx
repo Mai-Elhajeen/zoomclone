0aab0da0038e2a4bca77b3328747a61b8006fd5e
Mai-Elhajeen <maielhajeen2001>
2026-07-25 13:16:11 +0300

    Implement meeting room part with all features

--- app/(root)/(home)/layout.tsx
+++ app/(root)/(home)/layout.tsx
@@ -1,7 +1,13 @@
-import Navbar from '@/components/Navbar'
-import Sidebar from '@/components/Sidebar'
+import { Metadata } from 'next';
 import React, { ReactNode } from 'react'
 
+import Navbar from '@/components/Navbar'
+import Sidebar from '@/components/Sidebar'
+
+export const metadata: Metadata = {
+  title: 'Zoom Clone',
+  description: 'A workspace for your team, powered by Stream Chat and Clerk.',
+};
 
 const HomeLayout = ({children} : {children : ReactNode}) => {
   return (
@@ -9,13 +15,13 @@ const HomeLayout = ({children} : {children : ReactNode}) => {
         <Navbar />
         <div className='flex'>
             <Sidebar />
+            
             <section className='flex min-h-screen flex-1 flex-col overflow-x-hidden px-6 pb-6 pt-28 max-md:pb-14 sm:px-14'>
                 <div className='w-full'>
                     {children}
                 </div>
             </section>
         </div>
-        Footer
     </main>
   )
 }
